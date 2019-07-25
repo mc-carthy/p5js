d298addf420f1a08add49340225f0d25c1ae47d0
Mick McCarthy <mccarthy.mick0012@gmail.com>
2019-07-25 21:45:08 +0100

    Using 1D perlin noise

--- example/sketch.js
+++ example/sketch.js
@@ -1,7 +1,11 @@
+var xOff = 0;
 function setup() {
-
+    createCanvas(400, 400);
 }
 
 function draw() {
-
+    background(63);
+    xOff += 0.01;
+    var x = noise(xOff) * 400;
+    ellipse(x, 200, 40);
 }
