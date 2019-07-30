9d4a49e07d3dd6bf6d279acd2ada6c8908f19874
Mick McCarthy <mccarthy.mick0012@gmail.com>
2019-07-30 22:59:29 +0100

    Add second dimension to noise

--- example/sketch.js
+++ example/sketch.js
@@ -1,11 +1,16 @@
-var xOff = 0;
+var xOff1 = 0;
+var xOff2 = 100000;
+
+
 function setup() {
     createCanvas(400, 400);
 }
 
 function draw() {
     background(63);
-    xOff += 0.01;
-    var x = noise(xOff) * 400;
-    ellipse(x, 200, 40);
+    xOff1 += 0.01;
+    xOff2 += 0.01;
+    var x = noise(xOff1) * 400;
+    var y = noise(xOff2) * 400;
+    ellipse(x, y, 40);
 }
