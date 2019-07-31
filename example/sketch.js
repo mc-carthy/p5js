6c63eb9c595228b612b779a3efeebd70f866f64e
Mick McCarthy <mccarthy.mick0012@gmail.com>
2019-07-31 22:28:02 +0100

    Time variant flow field creation

--- example/sketch.js
+++ example/sketch.js
@@ -1,23 +1,31 @@
 var scl = 20;
-var inc = 0.02;
+var inc = 0.1;
+var t = 0;
 var cols, rows;
 
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(600, 600);
     col = floor(width / scl);
     row  = floor(height / scl);
 }
 
 function draw() {
+    background(255);
     var yOff = 0;
     for (y = 0; y < row; y++) {
         var xOff = 0;
         for (x = 0; x < col; x++) {
-            var value = random(255);
-            fill(value);
-            rect(x * scl, y * scl, scl, scl);
+            var value = noise(xOff, yOff, t);
+            var v = p5.Vector.fromAngle(value * TWO_PI);
+            stroke(0);
+            push();
+                translate(x * scl + scl / 2 , y * scl + scl / 2);
+                rotate(v.heading());
+                line(0, 0, scl / 2, 0);
+            pop();
             xOff += inc;
         }
         yOff += inc;
     }
+    t += 0.01
 }
