051cc508efc2c96b9503a57e60c9356086767d0e
Mick McCarthy <mccarthy.mick0012@gmail.com>
2019-07-31 21:52:06 +0100

    Create moving 2d perlin noise gradient

--- example/sketch.js
+++ example/sketch.js
@@ -1,20 +1,26 @@
-var inc = 0.01;
+var t = 0;
+var inc = 0.02;
 
 function setup() {
-    createCanvas(200, 200);
+    createCanvas(400, 400);
 }
 
 function draw() {
     loadPixels();
-    for (var x = 0; x < width; x++) {
-        for (var y = 0; y < height; y++) {
+    var yOff = 0;
+    for (var y = 0; y < height; y++) {
+        var xOff = 0;
+        for (var x = 0; x < width; x++) {
             var index = (x + y * width) * 4;
-            var value = random(255);
+            var value = noise(xOff, yOff, t) * 255;
             pixels[index + 0] = value;
             pixels[index + 1] = value;
             pixels[index + 2] = value;
             pixels[index + 3] = value;
+            xOff += inc;
         }
+        yOff += inc;
     }
+    t += inc;
     updatePixels();
 }
