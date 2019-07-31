0d6518ed25dc66feb93f50d60f5564c8b63246bb
Mick McCarthy <mccarthy.mick0012@gmail.com>
2019-07-31 22:15:38 +0100

    Create random grid ahead of flow field

--- example/sketch.js
+++ example/sketch.js
@@ -1,26 +1,23 @@
-var t = 0;
+var scl = 20;
 var inc = 0.02;
+var cols, rows;
 
 function setup() {
     createCanvas(400, 400);
+    col = floor(width / scl);
+    row  = floor(height / scl);
 }
 
 function draw() {
-    loadPixels();
     var yOff = 0;
-    for (var y = 0; y < height; y++) {
+    for (y = 0; y < row; y++) {
         var xOff = 0;
-        for (var x = 0; x < width; x++) {
-            var index = (x + y * width) * 4;
-            var value = noise(xOff, yOff, t) * 255;
-            pixels[index + 0] = value;
-            pixels[index + 1] = value;
-            pixels[index + 2] = value;
-            pixels[index + 3] = value;
+        for (x = 0; x < col; x++) {
+            var value = random(255);
+            fill(value);
+            rect(x * scl, y * scl, scl, scl);
             xOff += inc;
         }
         yOff += inc;
     }
-    t += inc;
-    updatePixels();
 }
