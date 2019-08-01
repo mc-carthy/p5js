069d79c8421c6f5f59b604191f1ae63944420cab
Mick McCarthy <mccarthy.mick0012@gmail.com>
2019-08-01 23:55:17 +0100

    Particles following flowfield

--- example/particle.js
+++ example/particle.js
@@ -2,10 +2,16 @@ function Particle() {
     this.pos = createVector(random(width), random(height));
     this.vel = createVector(0, 0);
     this.acc = createVector(0, 0);
+    this.maxSpeed = 5;
 
     this.update = function() {
         this.vel.add(this.acc);
+        this.vel.limit(this.maxSpeed);
         this.pos.add(this.vel);
+        if (this.pos.x > width) this.pos.x = 0;
+        if (this.pos.x < 0) this.pos.x = width;
+        if (this.pos.y > height) this.pos.y = 0;
+        if (this.pos.y < 0) this.pos.y = height;
         this.acc.mult(0);
     }
 
@@ -13,8 +19,17 @@ function Particle() {
         this.acc.add(force);
     }
 
+    this.follow = function(flowfield) {
+        var x = floor(this.pos.x / scl);
+        var y = floor(this.pos.y / scl);
+        var force = flowfield[x + y * col];
+        this.applyForce(force);
+    }
+
     this.show = function() {
          stroke(0);
+         strokeWeight(3);
          point(this.pos.x, this.pos.y);
+         strokeWeight(1);
     }
 }
--- example/sketch.js
+++ example/sketch.js
@@ -4,37 +4,57 @@ var t = 0;
 var cols, rows;
 
 var particles = [];
+var flowfield;
 
 function setup() {
     createCanvas(600, 600);
     col = floor(width / scl);
     row  = floor(height / scl);
+    flowfield = new Array(col * row);
+
     for (var i = 0; i < 100; i++) {
         particles[i] = new Particle();
     }
+
+    createFlowfield();
 }
 
-function draw() {
-    background(255);
+function createFlowfield() {
     var yOff = 0;
     for (y = 0; y < row; y++) {
         var xOff = 0;
         for (x = 0; x < col; x++) {
+            var index = x + y * col
             var value = noise(xOff, yOff, t);
             var v = p5.Vector.fromAngle(value * TWO_PI);
-            stroke(0);
+            flowfield[index] = v; 
+            xOff += inc;
+        }
+        yOff += inc;
+    }
+}
+
+function drawFlowfield() {
+    for (y = 0; y < row; y++) {
+        for (x = 0; x < col; x++) {
+            v = flowfield[x + y * col];
+            stroke(0, 50);
             push();
                 translate(x * scl + scl / 2 , y * scl + scl / 2);
                 rotate(v.heading());
                 line(0, 0, scl / 2, 0);
             pop();
-            xOff += inc;
         }
-        yOff += inc;
     }
-    t += 0.01
+}
+
+function draw() {
+    background(255);
+    drawFlowfield();
+    // t += 0.01
 
     for (var i = 0; i <particles.length; i++) {
+        particles[i].follow(flowfield); 
         particles[i].update();
         particles[i].show();
     }
